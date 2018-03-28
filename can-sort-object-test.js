e87db39c04f7f849cc1e98ba9f03facb67aea847
Justin Meyer <justinbmeyer@gmail.com>
2018-03-28 09:47:22 -0500

    first

---
+++ can-sort-object-test.js
@@ -0,0 +1,18 @@
+var sort = require("./can-sort-object");
+var QUnit = require("steal-qunit");
+
+QUnit.module("can-sort-object");
+
+QUnit.test('basics', function (assert) {
+    var obj = {
+        "z": 0,
+        "a": 0
+    };
+
+    var same = {
+        "a": 0,
+        "z": 0
+    };
+
+    assert.equal( JSON.stringify( sort(obj) ), JSON.stringify( same ) );
+});
